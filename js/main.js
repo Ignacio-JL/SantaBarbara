55ba232d40a71cceecbf115f3052bd52915ab8d9
Melissa Aime <aimemelissa1@gmail.com>
2022-04-27 23:03:06 -0300

    add cards to products

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+items.addEventListener('click', e => {
+    addCarrito(e)
+})
+
+const addCarrito = e => {
+    if (e.target.classList.contains('select-button')) {
+        //para crear el carrito.
+    }
+}
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('../products.json')
+        const data = await res.json()
+        //console.log(data);
+        bikiniCards(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const bikiniCards = data => {
+    data.forEach(producto => {
+        if (producto.type === "bikini") {
+            templateCard.querySelector('h5').textContent = producto.name.toUpperCase()
+            templateCard.querySelector('p').textContent = producto.price
+            templateCard.querySelector('img').setAttribute("src", producto.image)
+            templateCard.querySelector('button').dataset.id = producto.id
+    
+            const clone = templateCard.cloneNode(true)
+            fragment.appendChild(clone)
+        }         
+    })
+    items.appendChild(fragment)
+}
