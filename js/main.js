32b676473f5ecd82c7f505fecc435bc9651d029e
Melissa Aime <aimemelissa1@gmail.com>
2022-04-30 16:39:43 -0300

    home, navbar, footer, responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
 
+//Inicio Crear cards segun info el json
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('/js/stock.json')
+        const data = await res.json()
+        //console.log(data);
+        bikiniCards(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const bikiniCards = data => {
+    data.forEach(producto => {
+        if (producto.destacado) {
+            templateCard.querySelector('h5').textContent = producto.name.toUpperCase() 
+            templateCard.querySelector('p').textContent = 'AR$ ' + producto.price
+            templateCard.querySelector('#type').textContent = producto.type.toUpperCase()
+            templateCard.querySelector('img').setAttribute("src", producto.image[0])
+            templateCard.querySelector('button').dataset.id = producto.id
+    
+            const clone = templateCard.cloneNode(true)
+            fragment.appendChild(clone)
+        }         
+    })
+    items.appendChild(fragment)
+}
+//Fin crear cards
+
+
 class Producto{
     constructor(id, image, season, size, name, type, price, description){
         this.id = id;
@@ -80,6 +118,8 @@ document.getElementById("enteriza").onclick = () => {
 document.getElementById("conjunto").onclick = () => {
     mostrarProductos(productos.filter(e => e.type == 'comfy'));
 }
+
+
 
 
 
